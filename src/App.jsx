ce94ba88a07ccf658e64d624b3160482a1ac8bc5
Yulia <u.kaganovich@gmail.com>
2025-09-06 19:02:44 +0300

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { MainLayout } from "./components/MainLayout"
 
 function App() {
   
-  return <MainLayout />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<div>home</div>} />
+          <Route path="/forbidden" element={<div>forbidden</div>} /> 
+          <Route path="/addquestion" element={<div>add question</div>} />
+        </Route>
+      </Routes>
+      
+    </BrowserRouter>
+  )
 }
 
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+      <header>
+          <p>
+              <img src="" alt="react logo" />
+              <span>ReactCards</span>
+          </p>
+          <div>
+              <Button>
+                  
+              </Button>
+          </div>
+          
+    </header>
+  )
+}
+
+export default Header
